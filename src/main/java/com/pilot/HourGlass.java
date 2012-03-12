a66c48cfc1e6479f991ce5061f28cad5b0cfe907
Dmitry Rotenberg <mr.rotenberg@gmail.com>
2012-03-12 21:17:18 +0200

    changes in HourGlass

--- src/main/java/com/pilot/HourGlass.java
+++ src/main/java/com/pilot/HourGlass.java
@@ -10,18 +10,39 @@ package com.pilot;
 public class HourGlass {
     public static void main(String[] args) {
 
-        int[][] array = new int[5][5];
+        int[][] array = new int[10][10];
 
-        for(int i = 0; i < array.length; i++) {
+        int c = array.length / 2;
+        int r = array[c].length / 2;
 
-            for(int j = 0; j < array[i].length; j++) {
+        int step = 0;
 
-                if(i == j) {
-                    array[i][j] = 1;
-                    array[i][array[i].length - i - 1] = 1;
+        for(int i = array.length / 2; i < array.length; i++) {
+
+            if (r + step < array[c].length)  {
+
+                array[i][r + step] = 1;
+                array[i][r - step] = 1;
+                array[c - step][r + step] = 1;
+                array[c - step][r - step] = 1;
+                array[i][r] = 1;
+
+                if(array[0].length % 2 != 0)   {
+                    array[i - c][r] = 1;
+                    array[0][step] = 1;
+                    array[0][array[0].length - step - 1] = 1;
+
+                } else {
+                    array[i - c + 1][r] = 1;
+                    array[1][step] = 1;
+                    array[1][array[0].length - step - 1] = 1;
                 }
 
-                array[i][array[i].length / 2] = 1;
+
+                array[array.length - 1][step] = 1;
+                array[array.length - 1][array[0].length - step - 1] = 1;
+
+                step++;
             }
         }
 
